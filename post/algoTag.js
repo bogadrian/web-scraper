9ef97fe5cd80e42fe7fa3039b61b8e1227f547cc
Bogdan Adrian <bogdan4adrian@gmail.com>
2020-05-06 16:54:02 +0200

    Update algoTag.js

--- post/algoTag.js
+++ post/algoTag.js
@@ -1,37 +1,7 @@
 
 
 const arrayTag = [
-  tag1,
-  tag2,
-  tag3,
-  tag4,
-  tag5,
-  tag6,
-  tag7,
-  tag8,
-  tag9,
-  tag10,
-  tag11,
-  tag12,
-  tag13,
-  tag14,
-  tag15,
-  tag16,
-  tag17,
-  tag18,
-  tag19,
-  tag20,
-  tag21,
-  tag22,
-  tag23,
-  tag24,
-  tag25,
-  tag26,
-  tag27,
-  tag28,
-  tag29,
-  tag30,
-  tag31
+ 
 ];
 
 mapTag = {
